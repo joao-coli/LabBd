3fd41b8ecdc5ffd7fa7949ed26aa96ffdb7b1c15
joao-coli <joaog.coli@gmail.com>
2021-11-19 16:14:58 -0300

    Initial page

--- backend/labBd/views.py
+++ backend/labBd/views.py
@@ -6,9 +6,19 @@ from django.http import HttpResponse
 def index(request):
     """View principal do projeto"""
 
+    buttons = [
+        {'text': 'Cadastrar usuário', 'url': ''},
+        {'text': 'Cadastrar veículo', 'url': ''},
+        {'text': 'Procurar carona', 'url': ''},
+        {'text': 'Oferecer carona', 'url': ''},
+        {'text': 'Caronas realizadas', 'url': ''},
+        {'text': 'Pontos cadastrados', 'url': ''}
+    ]
+
     context = {
-        'teste' : 10
+        'buttons': buttons
     }
+
     return render(request, 'home.html', context=context)
 
 
